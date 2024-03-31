04a2dff0c19c6e26f0c236f1ff8a71bd422d78a7
kanak prajapati <prajapatikanak001@gmail.com>
2024-03-31 11:59:11 +0530

    some change

--- 6 rendom number.py
+++ 6 rendom number.py
@@ -1,4 +1,19 @@
-# Write a Python program to generate a random number.
+# Write a Python program to Random Password Generator.
 
 import random
+import string
+
 print(f"Rendom Number :- {random.randint(1, 100)}")
+
+a = int(input('How Many Pass You want ? : '))
+long = int(input('How Much Long pass you want ? : '))
+
+
+def pass_gen(length):
+    select = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
+    password = ''.join(random.choice(select) for _ in range(length)) 
+    return password
+
+for i in range(1, a+1):
+    generated = pass_gen(long)
+    print(f'Password {i} : {generated}')
